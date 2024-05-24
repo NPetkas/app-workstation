195cfd0703e4a9797beba22d6921b099f0ae36fa
rbaker4559 <reginaldsbaker10@icloud.com>
2024-05-23 21:42:20 -0700

    adding gitignore, env, and server skeleton

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
