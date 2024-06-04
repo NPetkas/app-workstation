0c56befab67402880904cf9018b55eb17b041baa
Nico P <nicopetkas@gmail.com>
2024-06-03 18:05:17 -0700

    css update

--- client/src/pages/SingleTask.jsx
+++ client/src/pages/SingleTask.jsx
@@ -2,13 +2,14 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
+
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
 
 import { QUERY_SINGLE_TASK } from '../utils/queries';
 
 const SingleTask = () => {
-  // Use `useParams()` to retrieve value of the route parameter `:profileId`
+  // Use `useParams()` to retrieve value of the route parameter `:taskId`
   const { taskId } = useParams();
 
   const { loading, data } = useQuery(QUERY_SINGLE_TASK, {
@@ -17,36 +18,38 @@ const SingleTask = () => {
   });
 
   const task = data?.task || {};
+  console.log('task', task)
 
   if (loading) {
     return <div>Loading...</div>;
   }
   return (
-    <div className="my-3">
-      <h3 className="card-header text-light p-2 m-0">
+    <div className="card-container my-3">
+     <div className="task-box bg-light py-4" style={{ border: '1px solid #1a1a1a' }}>
+      <h3 className="card-header-task text-light p-2 m-0" >
         {task.taskAuthor} <br />
-        <span style={{ fontSize: '1rem' }}>
+        <span style={{ fontSize: '1.25rem' }}>
           task created on {task.createdAt}
         </span>
       </h3>
-      <div className="bg-light py-4">
-        <blockquote
+      {/* <div className="task-box bg-light py-4" style={{ border: '1px solid #1a1a1a' }}> */}
+        <blockquote 
           className="p-4"
           style={{
             fontSize: '1.5rem',
             fontStyle: 'italic',
-            border: '2px dotted #1a1a1a',
+            border: '2px solid #1a1a1a',
             lineHeight: '1.5',
           }}
         >
-          {task.taskText}
+          {task.taskText} Task text should show up here
         </blockquote>
       </div>
 
       <div className="my-5">
         <CommentList comments={task.comments} />
       </div>
-      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+      <div className="comment-form m-3 p-4" style={{ border: '1px solid #1a1a1a' }}>
         <CommentForm taskId={task._id} />
       </div>
     </div>
--- client/src/pages/Task.jsx
+++ client/src/pages/Task.jsx
@@ -13,12 +13,13 @@ const Task = () => {
     <main>
       <div className="flex-row justify-center">
         <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          className="task col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px solid #1a1a1a'
+           }}
         >
           <TaskForm />
         </div>
-        <div className="col-12 col-md-8 mb-3">
+        <div className="task-list col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
