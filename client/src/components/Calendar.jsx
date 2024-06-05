6f7e71cf2df12ac2bc055aacbe1c70dd6e058cd6
Nico P <nicopetkas@gmail.com>
2024-06-05 14:37:59 -0700

    weather code update

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -6,7 +6,7 @@ const Calendar = () => {
 // how could i fix the style attribute for a jsx file?
 
 <iframe src="https://calendar.google.com/calendar/embed?height=500&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%23039BE5&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%230B8043" 
-style={{border: '2px solid black',
+style={{border: '1px solid black',
 borderRadius: '5px',
 boxShadow: '2px 3px 5px #000000'
 }}
