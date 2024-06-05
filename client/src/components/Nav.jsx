15ca30ef98c13d42e784c7edc119b87ed554cef7
rbaker4559 <reginaldsbaker10@icloud.com>
2024-06-04 20:39:04 -0700

    deleted console logs and created conditional for nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
 import Spotify from "./Spotify";
+import Auth from '../utils/auth'; 
 
-// nav bar
+// Nav bar
 export default function Nav() {
   const currentPage = useLocation().pathname;
+  const loggedIn = Auth.loggedIn();
 
   return (
     <nav className="nav">
@@ -11,50 +13,61 @@ export default function Nav() {
         <Spotify />
       </div>
       <div className="navers">
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={currentPage === "/" ? "nav-link active" : "nav-link"}
-          >
-            Profile
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/task"
-            className={currentPage === "/task" ? "nav-link active" : "nav-link"}
-          >
-            Tasks
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/note"
-            className={currentPage === "/note" ? "nav-link active" : "nav-link"}
-          >
-            Notes
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link
-            to="/logout"
-            className="nav-link"
-          >
-            Logout
-          </Link>
-        </li>
-        
-        <li className="nav-item">
-          <Link
-            to="/signup"
-            className={
-              currentPage === "/signup" ? "nav-link active" : "nav-link"
-            }
-          >
-            SignUp
-          </Link>
-        </li>
+        {loggedIn ? (
+          <>
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={currentPage === "/" ? "nav-link active" : "nav-link"}
+              >
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/task"
+                className={currentPage === "/task" ? "nav-link active" : "nav-link"}
+              >
+                Tasks
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/note"
+                className={currentPage === "/note" ? "nav-link active" : "nav-link"}
+              >
+                Notes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/logout"
+                className="nav-link"
+              >
+                Logout
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link
+                to="/signup"
+                className={currentPage === "/signup" ? "nav-link active" : "nav-link"}
+              >
+                SignUp
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/login"
+                className={currentPage === "/login" ? "nav-link active" : "nav-link"}
+              >
+                Login
+              </Link>
+            </li>
+          </>
+        )}
       </div>
     </nav>
   );
