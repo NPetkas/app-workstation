85949b7b84defcde0cd7a8886f4fecdb24bc3ddd
Nico <152521554+NPetkas@users.noreply.github.com>
2024-06-06 20:51:37 -0700

    Update Nav.jsx

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -78,52 +78,6 @@ export default function Nav() {
             </li>
           </>
         )}
-
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={currentPage === "/" ? "nav-link active" : "nav-link"}
-          >
-            Profile
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/task"
-            className={currentPage === "/task" ? "nav-link active" : "nav-link"}
-          >
-            Tasks
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/note"
-            className={currentPage === "/note" ? "nav-link active" : "nav-link"}
-          >
-            Notes
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link
-            to="/logout"
-            className="nav-link"
-          >
-            Logout
-          </Link>
-        </li>
-        
-        <li className="nav-item">
-          <Link
-            to="/signup"
-            className={
-              currentPage === "/signup" ? "nav-link active" : "nav-link"
-            }
-          >
-            SignUp
-          </Link>
-        </li>
-
       </div>
     </nav>
   );
